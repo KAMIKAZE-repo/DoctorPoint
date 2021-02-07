27bb7fefe77220da5922d7d468fecec8eaf3d668
KAMIKAZE-repo <nassimboussami@gmail.com>
2021-02-07 12:07:20 +0100

    Doctor calendar added

--- app/src/main/java/com/example/testingapp/searchfragment/doctordetails/DoctorDetailsActivity.kt
+++ app/src/main/java/com/example/testingapp/searchfragment/doctordetails/DoctorDetailsActivity.kt
@@ -1,20 +1,25 @@
 package com.example.testingapp.searchfragment.doctordetails
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testingapp.R
 import com.example.testingapp.appointment.bookappointment.BookAppointmentActivity
+import java.util.*
 
 class DoctorDetailsActivity : AppCompatActivity() {
     private lateinit var monthSelector: Spinner
     private lateinit var daysSelector: RecyclerView
     private lateinit var bookButton: Button
+    private val Dates = arrayListOf("MON", "TUE", "WEN", "THE", "FRI")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_details)
@@ -24,16 +29,13 @@ class DoctorDetailsActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         monthSelector.adapter = adapter
 
-        val data = arrayListOf(//TODO("implement time paring function")
-            "MON" to 10,
-            "TUE" to 11,
-            "WEN" to 12,
-            "THE" to 13,
-            "FRI" to 14,
-            "SAT" to 15
-        )
+        val data = datesGenerator()
         daysSelector  =findViewById(R.id.days_list)
-        daysSelector.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        daysSelector.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
         val daysRecyclerAdapter = DaysRecyclerAdapter(data) as RecyclerView.Adapter<*>
         daysSelector.adapter = daysRecyclerAdapter
 
@@ -45,5 +47,29 @@ class DoctorDetailsActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+    }
+
+
+    private fun datesGenerator():ArrayList<Pair<String, Int>>{
+        val result = arrayListOf<Pair<String, Int>>()
+        val dt = Date()
+        val c = Calendar.getInstance()
+        c.time = dt//date of today
+        val today = c.get(Calendar.DAY_OF_WEEK)
+        val i = if(today == 1){
+            c.add(Calendar.DATE, 1)
+            2
+        }else{
+            today
+        }
+        for(day in i until 7)
+        {
+            result.add(
+                Dates[day-2] to c.get(Calendar.DAY_OF_MONTH)
+            )
+            c.add(Calendar.DATE, 1)
+        }
+        Log.i("TAG", result.toString())
+        return result
     }
 }
